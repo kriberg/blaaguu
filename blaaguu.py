5d77b0c9e48ab0121e81a366c0cb1475aed28973
Kristian Berg <kriberg@tihlde.org>
2013-10-31 20:02:04 +0100

    Better post handling and some more stuff

--- blaaguu.py
+++ blaaguu.py
@@ -3,25 +3,45 @@ from datetime import datetime
 from UserDict import UserDict
 from markdown import markdown
 from git import Repo
+import json
 import os
 
 with open('secretkey.txt', 'r') as f:
     secretkey = f.read().strip()
+with open('projects.json', 'r') as f:
+    projects = json.loads(f.read())
 
 class PostHandler(UserDict):
+    repo = Repo('../blaagposts')
     def __init__(self):
         UserDict.__init__(self)
-        self.repo = Repo('../blaagposts')
         self.fetch_posts()
+
+    def parse_date(self, date):
+        return datetime.strptime(date,
+                "%a %b %d %H:%M:%S %Y")
 
     def fetch_posts(self):
         self.repo.git.pull()
         blobs = self.repo.heads.master.commit.tree.blobs
+        posts = []
         for b in blobs:
             if not b.name.lower().endswith('.md'):
                 continue
-            self[self.slug(b.name)] = (self.unslug(b.name),
-                    markdown(b.data_stream.read()))
+            dates = filter(lambda l: l.startswith('Date: '),
+                    self.repo.git.log(b.name).splitlines())
+            dates = [t[5:-5].strip() for t in dates]
+            posted = self.parse_date(dates[-1])
+            last_updated = self.parse_date(dates[0])
+            post_data = {'title': self.unslug(b.name),
+                    'slug': self.slug(b.name),
+                    'posted': posted,
+                    'last_updated': last_updated,
+                    'text': markdown(b.data_stream.read())}
+            posts.append((posted, post_data))
+            self[self.slug(b.name)] = post_data
+        self.posts = sorted(posts, key=lambda x: x[0])
+        self.posts.reverse()
 
     def slug(self, s):
         return s.replace('.md','').replace(' ', '-').title()
@@ -35,7 +55,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', ph=ph)
+    return render_template('index.html',
+            ph=ph,
+            projects=projects)
 
 @app.route('/{0}'.format(secretkey))
 def reload():
